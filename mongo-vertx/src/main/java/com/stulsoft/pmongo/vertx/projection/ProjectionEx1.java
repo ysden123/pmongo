074f5722cc00645344eb203d6f8e7fbc5241345d
Yuriy Stul <yuriy.s@webpals.com>
2021-04-21 13:57:37 +0300

    projection

--- mongo-vertx/src/main/java/com/stulsoft/pmongo/vertx/projection/ProjectionEx1.java
+++ mongo-vertx/src/main/java/com/stulsoft/pmongo/vertx/projection/ProjectionEx1.java
@@ -25,12 +25,21 @@ public class ProjectionEx1 {
                 logger.debug("(1) {}", ar.result());
             });
 
-            client.find("test1", new JsonObject().put("b","bbb"), ar -> {
+            client.find("test1", new JsonObject().put("b", "bbb"), ar -> {
                 logger.debug("(2) {}", ar.result());
             });
+// todo YS
+            var findOptions=new FindOptions()
+                    .setSort(new JsonObject().put("aaa",-1))
+                    .setBatchSize(1000);
 
+            client.findBatchWithOptions("collection", new JsonObject(), findOptions)
+                    .handler(document -> logger.debug("{}", document.encode()))
+                    .exceptionHandler(error -> logger.error(error.getMessage()))
+                    .endHandler(__ -> logger.debug("Completed"));
+// todo YS
             client.findWithOptions("test1", new JsonObject(),
-                    new FindOptions().setFields(new JsonObject().put("b", 0)), ar->{
+                    new FindOptions().setFields(new JsonObject().put("b", 0)), ar -> {
                         logger.debug("(3) {}", ar.result());
                     });
         });
