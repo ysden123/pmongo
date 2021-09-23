459e02f5085ca46cfad68323813aac0263f7d2e9
Yuriy Stul <ysden123@gmail.com>
2021-09-23 17:26:23 +0300

    Find a last item

--- jmongo/src/main/java/com/stulsoft/pmongo/jmongo/versioning/ReadLastVersionData.java
+++ jmongo/src/main/java/com/stulsoft/pmongo/jmongo/versioning/ReadLastVersionData.java
@@ -17,9 +17,13 @@ import org.slf4j.LoggerFactory;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Sorts.ascending;
+import static com.mongodb.client.model.Sorts.descending;
 
 /**
  * @author Yuriy Stul
@@ -27,12 +31,19 @@ import static com.mongodb.client.model.Aggregates.*;
 public class ReadLastVersionData {
     private static final Logger logger = LoggerFactory.getLogger(ReadLastVersionData.class);
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main(String[] args) {
         logger.info("==>main");
 
-        test1();
-        test2();
-        test3();
+        try {
+            test1();
+            test2();
+            test3();
+            test4();
+        }catch(Exception exception){
+            logger.error(exception.getMessage(), exception);
+        }
     }
 
     private static void test1() {
@@ -55,7 +66,7 @@ public class ReadLastVersionData {
                         )
                 )
                 .forEach(document -> logger.info("{}", document));
-        logger.info("<==test1 in {}",System.currentTimeMillis() - start);
+        logger.info("<==test1 in {}", System.currentTimeMillis() - start);
         client.close();
     }
 
@@ -83,7 +94,7 @@ public class ReadLastVersionData {
         map.values().stream().sorted(Comparator.comparing(TestAggCollection::a))
                 .collect(Collectors.toList())
                 .forEach(testAggCollection -> logger.info("{}", testAggCollection));
-        logger.info("<==test2 in {}",System.currentTimeMillis() - start);
+        logger.info("<==test2 in {}", System.currentTimeMillis() - start);
         client.close();
     }
 
@@ -96,13 +107,43 @@ public class ReadLastVersionData {
         var map = new HashMap<Double, Document>();
         collection
                 .find(Document.class)
-                .sort(Sorts.descending("a", "t"))
                 .allowDiskUse(true)
+                .sort(ascending("a", "t"))
                 .forEach(document -> map.put(document.getDouble("a"), document));
         map.values().stream().sorted(Comparator.comparing(o -> o.getDouble("a")))
+                .map(document -> {
+                    try{
+                        return objectMapper.readValue(document.toJson(), TestAggCollection.class);
+                    }catch(Exception exception){
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList())
-                .forEach(document -> logger.info("{}", document));
-        logger.info("<==test in {}",System.currentTimeMillis() - start);
+                .forEach(testAggCollection -> logger.info("{}", testAggCollection));
+        logger.info("<==test3 in {}", System.currentTimeMillis() - start);
         client.close();
+    }
+
+    /**
+     * Find a last item
+     */
+    private static void test4() throws JsonProcessingException {
+        logger.info("==>test4");
+        var client = Client.client();
+        var db = client.getDatabase("testDb");
+        var collection = db.getCollection("testAggCollection");
+
+        var document = collection
+                .find(eq("a", 2.0))
+                .allowDiskUse(true)
+                .sort(descending("a", "t"))
+                .first();
+        if (document != null){
+            logger.info("{}", objectMapper.readValue(document.toJson(), TestAggCollection.class));
+        }else{
+            logger.info("Did not found");
+        }
+        logger.info("<==test4");
     }
 }
